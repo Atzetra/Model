60e363541097483170f3c1a3af0b9e24dfc6f14e
Enzo Sastrokarijo <enzo@sastrokarijo.nl>
2022-06-06 21:27:36 +0200

    Complete steady state model

--- model.r
+++ model.r
@@ -5,10 +5,18 @@ library(tidyverse)
 
 #################
 # Model parameters
-ks <- 0.001
+ks <- 0.1987
 # synthesis rate of NAPE
 kdeg <- 0.001
 # degradation rate of PA
+kpa <- 0.6
+# basal synthesis of PA
+kpea <- 0.2953
+# basal degredation of PEA
+kass <- 0.005
+# assosiation constant of PPAR
+kdis <- 0.005
+# dissasociation of PPARa
 Vmpld <- 97.9
 # vmax for NAPE to PEA conversion by PLD enzyme
 Kmpld <- 3300
@@ -31,6 +39,10 @@ Kmna <- 97000
 para <- unlist(c(data.frame(
     ks,
     kdeg,
+    kass,
+    kdis,
+    kpa,
+    kpea,
     Vmpld, # vmax for NAPE to PEA conversion by PLD enzyme
     Kmpld, # Km for NAPE to PEA conversion by PLD enzyme
     Vmgde, # vmax for NAPE to PEA conversion by GDE1_4 enzyme
@@ -45,22 +57,26 @@ para
 PEA_model <- function(t, x, parms) {
     with(as.list(c(parms, x)), {
         dNAPE <- ks - Vmpld * NAPE / (Kmpld + NAPE) - Vmgde * NAPE / (Kmgde + NAPE)
-        dPEA <- Vmpld * NAPE / (Kmpld + NAPE) + Vmgde * NAPE / (Kmgde + NAPE) - Vmfa * PEA / (Kmfa + PEA) - Vmna * PEA / (Kmna + PEA)
-        dPA <- Vmfa * PEA / (Kmfa + PEA) + Vmna * PEA / (Kmna + PEA) - kdeg * PA
+        dPEA <- Vmpld * NAPE / (Kmpld + NAPE) + Vmgde * NAPE / (Kmgde + NAPE) - Vmfa * PEA / (Kmfa + PEA) - Vmna * PEA / (Kmna + PEA) - kass * PEA + kdis * PPRA - kpea * PEA
+        dPA <- kpa + Vmfa * PEA / (Kmfa + PEA) + Vmna * PEA / (Kmna + PEA) - kdeg * PA
+        dPPRA <- kass * PEA - kdis * PPRA
 
-        res <- c(dNAPE, dPEA, dPA)
+        res <- c(dNAPE, dPEA, dPA, dPPRA)
         list(res)
     })
 }
 
 t <- seq(0, 240, 1)
 xstart <- c(
-    NAPE = 0.03370793,
-    PEA = 1.50915694,
-    PA = 1.00002487
+    NAPE = 0.3371102,
+    PEA = 14.0134102,
+    PA = 48.9999720,
+    PPRA = 70.0670294
 )
 
 out <- ode(y = xstart, times = t, func = PEA_model, parms = para)
 plot(out)
 
 RS <- runsteady(y = xstart, func = PEA_model, parms = para, times = c(0, 1e18))
+
+unlist(RS)
